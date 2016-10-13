ce2390e8131402d5bb661cfbc0615c7a9674eb8c
Joost de Ruijter <joost1235@hotmail.com>
2016-10-13 12:25:23 +0200

    added user model

--- src/main/java/com/hr/securitylab/database/models/User.java
+++ src/main/java/com/hr/securitylab/database/models/User.java
@@ -1,19 +1,52 @@
 package com.hr.securitylab.database.models;
 
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by joost on 4-10-2016.
  */
+
+@Entity
+@Table(name = "user")
 public class User {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "created_at")
+    private Date created_at;
+
+    @Column(name = "updated_at")
+    private Date updated_at;
+
+    @Column(name = "deleted_at")
+    private Date deleted_at;
 
     public User() {
     }
 
-    public User(String username, String password) {
+    public User(String username, String password, Date created_at, Date updated_at, Date deleted_at) {
         this.username = username;
         this.password = password;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.deleted_at = deleted_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -30,5 +63,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Date getDeleted_at() {
+        return deleted_at;
+    }
+
+    public void setDeleted_at(Date deleted_at) {
+        this.deleted_at = deleted_at;
     }
 }
